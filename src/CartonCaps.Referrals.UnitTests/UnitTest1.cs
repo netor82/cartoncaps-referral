e77c057465f1f529832559ffb1e9ab0fa283f005
Ernesto Rivera <53798130+netor82@users.noreply.github.com>
2026-01-25 14:29:53 -0600

    Tests

---
+++ src/CartonCaps.Referrals.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace CartonCaps.Referrals.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
